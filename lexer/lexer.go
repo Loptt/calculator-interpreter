96f5f6fddc52b966e9faf7305a59bfc23bdb8121
Carlos Estrada <carlos.deg02@hotmail.com>
2020-02-11 19:59:03 -0600

    Added scan function, tests still failing

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,9 @@
 package lexer
 
+import (
+	"strings"
+)
+
 type TokenType int
 
 const (
@@ -13,6 +17,10 @@ type Token struct {
 	val string
 }
 
+func (t Token) Equal(t2 Token) bool {
+	return t.tt == t2.tt && t.val == t2.val
+}
+
 var transMatrix [][]int = [][]int{
 	{1, 2, 3, 0},
 	{1, 101, 101, 101},
@@ -20,14 +28,44 @@ var transMatrix [][]int = [][]int{
 	{103, 103, 103, 103},
 }
 
-func scan(s string) []Token {
-	//index := 0
-	return []Token{}
+func Scan(s string) []Token {
+	s = s + " "
+	index := 0
+	state := 0
+
+	var value strings.Builder
+	var tokens []Token
+
+	var curr byte
+
+	for index < len(s) {
+		for state < 100 && index < len(s) {
+			curr = s[index]
+			state = transMatrix[state][filter(curr)]
+			if curr != ' ' {
+				value.WriteByte(curr)
+			}
+			index++
+		}
+
+		switch state {
+		case 101:
+			tokens = append(tokens, Token{Number, value.String()})
+		case 102:
+			tokens = append(tokens, Token{Plus, value.String()})
+		case 103:
+			tokens = append(tokens, Token{Minus, value.String()})
+		}
+
+		state = 0
+		value.Reset()
+	}
+	return tokens
 }
 
-func filter(c rune) int {
+func filter(c byte) int {
 	switch c {
-	case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':  
+	case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
 		return 0
 	case '+':
 		return 1
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,10 +1,14 @@
 package lexer
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestFilter(t *testing.T) {
 	tests := []struct {
-		in   rune
+		in   byte
 		want int
 	}{
 		{'1', 0},
@@ -21,6 +25,36 @@ func TestFilter(t *testing.T) {
 		got := filter(test.in)
 		if got != test.want {
 			t.Errorf("Filter(%c) = %d, expected %d", test.in, got, test.want)
+		}
+	}
+}
+
+func TestScan(t *testing.T) {
+	tests := []struct {
+		in   string
+		want []Token
+	}{
+		{
+			in: "2 3",
+			want: []Token{
+				Token{Number, "2"},
+				Token{Number, "3"},
+			},
+		},
+		{
+			in: "1+4",
+			want: []Token{
+				Token{Number, "1"},
+				Token{Plus, "+"},
+				Token{Number, "4"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		got := Scan(test.in)
+		if !cmp.Equal(got, test.want) {
+			t.Errorf("Scan(%v) = %v, expected %v", test.in, got, test.want)
 		}
 	}
 }
